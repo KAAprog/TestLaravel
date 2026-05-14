91d5a4e763509ae650b9acce77576f17964b38ff
KAAprog <adelinekana778@gmail.com>
2026-05-14 16:52:28 +0100

    feat:mise a jour du modele salle

--- app/Models/Salle.php
+++ app/Models/Salle.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Salle extends Model
 {
-    //
-}
+   use HasFactory;
+
+   protected $fillable = ['nom_salle', 'capacite', 'batiment'];
+   protected $attributes = ['capacite' => 30];
+   }

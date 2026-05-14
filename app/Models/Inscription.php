5757c8ae2757668cc4a977036a9fc406559d95f4
KAAprog <adelinekana778@gmail.com>
2026-05-14 16:05:37 +0100

    feat:mise a jour du modele inscription

--- app/Models/Inscription.php
+++ app/Models/Inscription.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Inscription extends Model
 {
-    //
+    protected $fillable = ['date_inscription', 'etudiant_id', 'specialite_id','niveau_id', 'annee_academique_id',     ];
+
+    protected $casts = ['date_inscription' => 'date'];
+
+    public function etudiant(): BelongsTo   {
+        return $this>belongsTo(Etudiant::class);
+    }
+    public function specialite(): BelongsTo {
+        return $this>belongsTo(Specialite::class);
+}
+    public function niveau(): BelongsTo
+    {
+        return $this>belongsTo(Niveau::class); }
+    public function anneeAcademique(): BelongsTo
+    {
+        return $this->belongsTo(AnneeAcademique::class);
+     }
 }

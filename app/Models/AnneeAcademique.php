bb1525f304661d34fc76184041ef3747f64572bf
KAAprog <adelinekana778@gmail.com>
2026-05-14 16:25:26 +0100

    feat: mise a jour du odele anneAcademique

--- app/Models/AnneeAcademique.php
+++ app/Models/AnneeAcademique.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnneeAcademique extends Model
 {
-    //
+ protected $table = 'annees_academiques';
+ protected $fillable = ['libelle', 'date_debut', 'date_fin'];
+ protected $casts = ['date_debut' => 'date', 'date_fin' => 'date'];
 }

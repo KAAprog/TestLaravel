5308773d1d189b080cd0de006a050a68893dc438
KAAprog <adelinekana778@gmail.com>
2026-05-14 15:09:35 +0100

    feat:mise a jour du mode de module

--- app/Models/Cours.php
+++ app/Models/Cours.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cours extends Model
 {
-    //
+     protected $fillable = ['nom_module', 'volume_horaire', 'specialite_id', 'semestre_id'];
+
+     public function specialite(): BelongsTo  {
+        return $this>belongsTo(Specialite::class); }
+     public function semestre(): BelongsTo    {
+         return $this>belongsTo(Semestre::class); }
+     public function cours(): HasMany          {
+        return $this>hasMany(Cours::class); }
 }

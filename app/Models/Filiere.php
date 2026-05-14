e6a0b2959550fe5e5528daa83681b89b7d4861cf
KAAprog <adelinekana778@gmail.com>
2026-05-14 14:52:36 +0100

    feat:mise a jour du model filiere

--- app/Models/Filiere.php
+++ app/Models/Filiere.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Filiere extends Model
 {
-    //
+     use HasFactory;
+    // Colonnes que l'on autorise à remplir en masse (create, update)
+     protected $fillable = ['nom_filiere'];
+     //Une filière possède plusieurs spécialités
+     public function specialites(): HasMany { return $this->hasMany(Specialite::class); }
 }

d12f41997c22395b8743032afb305853b20aff7a
KAAprog <adelinekana778@gmail.com>
2026-05-14 15:53:41 +0100

    feat:mise a jour du modele etudiant

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Etudiant extends Model
 {
-    //
+   protected $fillable = ['nom', 'prenom', 'matricule', 'email', 'telephone'];
+   
+   public function inscriptions(): HasMany {
+    return $this>hasMany(Inscription::class); }
+   public function pointages(): HasMany    {
+    return $this>hasMany(Pointage::class); }
+   public function getNomCompletAttribute(): string     {
+    return $this->prenom . ' ' . $this->nom;
+   }
 }

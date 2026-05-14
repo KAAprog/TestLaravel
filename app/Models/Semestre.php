dff3ca3d957eb71907b8fe94bf7f8a702ca71f0c
KAAprog <adelinekana778@gmail.com>
2026-05-14 16:44:26 +0100

    feat:mise a jour du modele semestre

--- app/Models/Semestre.php
+++ app/Models/Semestre.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Semestre extends Model
 {
-    //
+  protected $fillable = [
+        'libelle',
+        'numero',
+        'niveau_id'
+    ];
+
+    public function niveau(): BelongsTo
+    {
+        return $this->belongsTo(Niveau::class);
+    }
 }

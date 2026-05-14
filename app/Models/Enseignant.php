37e765c41fc78ade0b0c271319e424273965e2ab
KAAprog <adelinekana778@gmail.com>
2026-05-14 15:38:24 +0100

    feat:mise a jour du modele enseignant

--- app/Models/Enseignant.php
+++ app/Models/Enseignant.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Enseignant extends Model
 {
-    //
+   protected $fillable = ['nom', 'prenom', 'email', 'telephone', 'specialite_ens'];
+
+   public function cours(): BelongsToMany
+   {
+     return $this->belongsToMany(Cours::class, 'enseigner');
+       }
+    public function seances(): HasMany
+    {
+        return $this->hasMany(Seance::class);
+        }
+    public function getNomCompletAttribute(): string     {
+        return $this->prenom . ' ' . $this->nom;
+        }
 }

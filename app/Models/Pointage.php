35bd8e143d5062a02ce3d1fd127788445f9993ff
KAAprog <adelinekana778@gmail.com>
2026-05-14 16:19:42 +0100

    feat:mise a jour du modele pointage

--- app/Models/Pointage.php
+++ app/Models/Pointage.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pointage extends Model
 {
-    //
+   protected $fillable = ['statut', 'observation', 'date_pointage', 'seance_id', 'etudiant_id'];
+
+   protected $casts = ['date_pointage' => 'datetime'];
+   
+   public function seance(): BelongsTo
+{
+   return $this>belongsTo(Seance::class);
+   }
+   public function etudiant(): BelongsTo
+   {
+    return $this>belongsTo(Etudiant::class);
+   }
 }

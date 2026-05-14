31566f81c123e196eb45938d0aa2b1cdf3ae0de9
KAAprog <adelinekana778@gmail.com>
2026-05-14 14:57:57 +0100

    feat:mise a jour  du modele de specialite

--- app/Models/Specialite.php
+++ app/Models/Specialite.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Specialite extends Model
 {
-    //
+   protected $fillable = ['nom_specialite', 'filiere_id'];
+   public function filiere(): BelongsTo     {
+    return $this->belongsTo(Filiere::class);
+     }
+   public function modules(): HasMany     {
+    return $this->hasMany(Module::class);
+    }
+   public function inscriptions(): HasMany     {
+     return $this->hasMany(Inscription::class);
+    }
 }
